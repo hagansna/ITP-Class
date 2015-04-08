c25d3fe00863db8a0b79c628154c97e27f0d9f1e
Nicholas Hagans <nah364@nyu.edu>
2015-04-08 13:30:02 -0400

    Adds documentation to instagram1 javascript file

--- project1/instagram1.js
+++ project1/instagram1.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+  // array of epoch times (from April 1st to April 6th)
   var times = [1427904000,1427990400,1428076800,1428163200,1428249600,1428336000];
+  // loop through each timestamp and use time in api call
   $.each(times, function(index, value) {
     var url = 'https://api.instagram.com/v1/media/search?max_timestamp='+value+'&lat=40.7300&lng=-73.9950&client_id=API_KEY'
+    // use ajax to get data from instagram
     $.ajax({
       url: url,
       dataType: 'jsonp',
@@ -9,7 +12,7 @@ $(document).ready(function() {
         console.log(jsonData.data);
 
         var pics = jsonData.data;
-
+        // for each picture, append the picture to the container div with height and width scaled based on likes/tagmeasure ratio.
         for(var i=0;i<pics.length;i++) {
           var likes = pics[i].likes.count;
           var tags = pics[i].tags.length;
@@ -22,6 +25,7 @@ $(document).ready(function() {
       }
     });
   });
+  // adds tooltip functionality that reads image title.
   $(function() {
     $(document).tooltip();
   });
