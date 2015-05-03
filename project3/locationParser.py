b5546d0b15246ccd2485bdb1b0eaad888cc5f641
Nicholas Hagans <nah364@nyu.edu>
2015-05-03 14:31:55 -0400

    initial commit

---
+++ project3/locationParser.py
@@ -0,0 +1,25 @@
+import re
+import json
+
+f = open('cameraLocations.txt', 'r+')
+file = f.read()
+
+latlngs = re.findall('([\d.-]+)", "([\d.-]+)', file)
+streets = re.findall('c_html\d+ = "([\d\w\s(?@)(?\-)(?.)]+)', file)
+# images = re.findall("img src=\'([\w\-(?\d)./(?:)]+)\'", "file")
+latlng = []
+for tup in latlngs:
+    latlng.append((float(tup[0]), float(tup[1])))
+
+location = {}
+for i in range(len(latlng)):
+    try:
+        location["location"+str(i)] = [latlng[i][0], latlng[i][1], streets[i]]
+    except Exception:
+        location["location"+str(i)] = "No Street Name Found?"
+f.close()
+
+location = json.dumps(location)
+f = open('location.txt', 'w+')
+f.write(location)
+f.close()
